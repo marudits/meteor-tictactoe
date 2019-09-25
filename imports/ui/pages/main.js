eed90350d5594d745d55a8c4b6048c2046eeedb0
marudits <marudits@gmail.com>
2019-09-26 00:44:17 +0700

    :sparkles: NEW FEATURES | Add isVerticallyWin checking

--- imports/ui/pages/main.js
+++ imports/ui/pages/main.js
@@ -1,7 +1,7 @@
 import { Template } from 'meteor/templating';
 
 // api : Boards
-// import { Boards } from '../../api/boards.js';
+import { Boards } from '../../api/boards.js';
 
 // ui : components
 import '../components/board.js';
@@ -11,9 +11,11 @@ import '../components/buttonInit.js';
 import './main.html';
 
 Template.body.helpers({
-  tasks: [
-    { text: 'This is task 1' },
-    { text: 'This is task 2' },
-    { text: 'This is task 3' },
-  ],
+  status() {
+    const board = Boards.findOne(FlowRouter.getParam('board_id'));
+
+    if (board) {
+        return board.status;
+    }
+  }
 });
